a72559fa7e99ef51181ba64fbe768a68c19417e3
Nick Rosa <nicholascastrorosa@gmail.com>
2020-01-14 16:40:54 -0700

    Finished, looking at node messed with me on this one.

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -34,11 +34,9 @@ class EvenAndOdd extends Component {
         return (
          <div className="puzzleBox evenAndOddPB">
             <h4> Evens and Odds </h4>
-            <input 
-            className="inputLine" 
+            <input className="inputLine" 
             onChange={ (e) => this.handleChange(e.target.value)}></input>
-            <button 
-            className="confirmationButton" 
+            <button className="confirmationButton" 
             onClick={ () => {this.assignEvensAndOdds(this.state.userInput)} }>Split</button>
             <span className="resultsBox"> Evens: {JSON.stringify(this.state.evenArray)} </span>
             <span className="resultsBox"> Odds: {JSON.stringify(this.state.oddArray)} </span>
